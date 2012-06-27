23b1c82691a62aab3fd5a279abc924e03c06b8dd
Charlie Stigler <charlie@charliestigler.com>
2012-06-27 14:03:14 -0700

    Initial stub, with structure hugely based on Don Park's hbs

---
+++ express-dot.js
@@ -0,0 +1,54 @@
+var fs = require('fs');
+var path = require('path');
+var doT = require('dot');
+
+var _cache = {};
+
+function _renderFile(locals, cb) {
+  var template = _cache[filename];
+  if (template) {
+    return cb(null, template(locals));
+  }
+
+  return fs.readFile(filename, 'utf8', function(err, str){
+    if (err) return cb(err);
+
+    var locals = options;
+    var template = doT.template(str);
+    if (options.cache) _cache[filename] = template;
+    return cb(null, template(locals));
+  });
+}
+
+function renderWithLayout(template, locals, cb) {
+  return _renderFile(locals, function(err, str) {
+    if (err) return cb(err);
+
+    var locals = options;
+    locals.body = str;
+    var str = template(locals);
+    return cb(null, str);
+  });
+}
+
+exports.__express = function(filename, options, cb) {
+    var extension = path.extname(filename);
+
+    if (options.layout !== undefined && !options.layout) return _renderFile(options, cb);
+
+    var viewDir = options.settings.views;
+    var layoutFileName = path.join(viewDir, options.layout || 'layout' + extension);
+
+    var layoutTemplate = _cache[layoutFileName];
+    if (layoutTemplate) return renderWithLayout(layoutTemplate, options, cb);
+
+    return fs.readFile(layoutFileName, 'utf8', function(err, str) {
+      if (err) return cb(err);
+
+      var layoutTemplate = doT.template(str);
+      if (options.cache) _cache[layoutFileName] = layoutTemplate;
+
+      return renderWithLayout(layoutTemplate, options, cb);
+    });
+  }
+};
