58523d8f17702b9fa15443717c79b47bd413f88b
Charlie Stigler <charlie@charliestigler.com>
2012-07-03 02:58:21 -0700

    added globals support

--- express-dot.js
+++ express-dot.js
@@ -1,53 +1,64 @@
 var fs = require('fs');
 var path = require('path');
 var doT = require('dot');
+var async = require('async');
 
 var _cache = {};
+var _globals = {};
 
-function _renderFile(locals, cb) {
+function _renderFile(filename, options, cb) {
+  'use strict';
+  cb = (typeof cb === 'function') ? cb : function () {};
+
   var template = _cache[filename];
   if (template) {
-    return cb(null, template(locals));
+    return cb(null, template.call(_globals, options));
   }
 
-  return fs.readFile(filename, 'utf8', function(err, str){
+  return fs.readFile(filename, 'utf8', function (err, str) {
     if (err) return cb(err);
 
-    var locals = options;
-    var template = doT.template(str);
+    var template = doT.template(str, null, _globals);
     if (options.cache) _cache[filename] = template;
-    return cb(null, template(locals));
+    return cb(null, template.call(_globals, options));
   });
 }
 
-function renderWithLayout(template, locals, cb) {
-  return _renderFile(locals, function(err, str) {
+function _renderWithLayout(filename, layoutTemplate, options, cb) {
+  'use strict';
+  cb = (typeof cb === 'function') ? cb : function () {};
+
+  return _renderFile(filename, options, function (err, str) {
     if (err) return cb(err);
-
-    var locals = options;
-    locals.body = str;
-    var str = template(locals);
-    return cb(null, str);
+    options.body = str;
+    return cb(null, layoutTemplate.call(_globals, options));
   });
 }
+
+exports.setGlobals = function(globals) {
+  'use strict';
+  _globals = globals;
+};
 
 exports.__express = function(filename, options, cb) {
+    'use strict';
+    cb = (typeof cb === 'function') ? cb : function () {};
     var extension = path.extname(filename);
 
-    if (options.layout !== undefined && !options.layout) return _renderFile(options, cb);
+    if (options.layout !== undefined && !options.layout) return _renderFile(filename, options, cb);
 
     var viewDir = options.settings.views;
     var layoutFileName = path.join(viewDir, options.layout || 'layout' + extension);
 
     var layoutTemplate = _cache[layoutFileName];
-    if (layoutTemplate) return renderWithLayout(layoutTemplate, options, cb);
+    if (layoutTemplate) return _renderWithLayout(filename, layoutTemplate, options, cb);
 
-    return fs.readFile(layoutFileName, 'utf8', function(err, str) {
+    return fs.readFile(layoutFileName, 'utf8', function (err, str) {
       if (err) return cb(err);
 
-      var layoutTemplate = doT.template(str);
+      var layoutTemplate = doT.template(str, null, _globals);
       if (options.cache) _cache[layoutFileName] = layoutTemplate;
 
-      return renderWithLayout(layoutTemplate, options, cb);
+      return _renderWithLayout(filename, layoutTemplate, options, cb);
     });
 };
